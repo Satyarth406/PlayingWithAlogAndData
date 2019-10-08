b52abde305dafd0eb83a501a7274584b207cdac3
SATYARTH MISRA <misrasatyarth@gmail.com>
2019-10-08 17:40:06 +0530

    added doubly link list project and few of its methods

---
+++ DoublyLinkedList/DoublyLinkedListSM.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoublyLinkedList
+{
+    public class DoublyLinkedListSM
+    {
+        public DoublyLinkedListNodeSM Head;
+        public DoublyLinkedListNodeSM Last;
+
+        public DoublyLinkedListSM()
+        {
+            Head = null;
+            Last = null;
+        }
+
+        public void AddANodeInTheEnd(int data)
+        {
+            DoublyLinkedListNodeSM doublyLinkedListNodeSM = new DoublyLinkedListNodeSM(data);
+            if (Head == null)
+            {
+                Head = doublyLinkedListNodeSM;
+                Last = doublyLinkedListNodeSM;
+                return;
+            }
+            doublyLinkedListNodeSM.Prev = Last;
+            Last.Next = doublyLinkedListNodeSM;
+            Last = doublyLinkedListNodeSM;
+        }
+        public void AddANodeAtFirst(int data)
+        {
+            DoublyLinkedListNodeSM doublyLinkedListNodeSM = new DoublyLinkedListNodeSM(data);
+            if (Head == null)
+            {
+                Head = doublyLinkedListNodeSM;
+                Last = doublyLinkedListNodeSM;
+                return;
+            }
+            doublyLinkedListNodeSM.Next = Head;
+            Head.Prev = doublyLinkedListNodeSM;
+            Head = doublyLinkedListNodeSM;
+        }
+
+        public void PrintDoublyLinkedList()
+        {
+            while (Head != null)
+            {
+                Console.WriteLine(Head.Data);
+                Head = Head.Next;
+            }
+        }
+
+        public void AddANodeAfterAGivenData(int d, int a)
+        {
+            DoublyLinkedListNodeSM doublyLinkedListNodeSM = null;
+            DoublyLinkedListNodeSM nodeToAdd = new DoublyLinkedListNodeSM(a);
+
+            DoublyLinkedListNodeSM dummyHead = Head;
+            while (dummyHead != null)
+            {
+                if (dummyHead.Data == d)
+                {
+                    break;
+                }
+                dummyHead = dummyHead.Next;
+            }
+            if (dummyHead == null)
+            {
+                Console.WriteLine("There is no node with the given data");
+                return;
+            }
+            nodeToAdd.Next = dummyHead.Next;
+            nodeToAdd.Prev = dummyHead;
+            dummyHead.Next.Prev = nodeToAdd;
+            dummyHead.Next = nodeToAdd;
+
+        }
+
+        public void DeleteANode(int d)
+        {
+            DoublyLinkedListNodeSM nodeToDelete = Head;
+            
+            while (nodeToDelete != null && nodeToDelete.Data != d)
+            {
+                nodeToDelete = nodeToDelete.Next;
+            }
+            if (nodeToDelete == null)
+            {
+                Console.WriteLine("The given data isnt part of doubly linked list");
+                return;
+            }
+            
+            nodeToDelete.Prev.Next = nodeToDelete.Next;
+            nodeToDelete.Next.Prev = nodeToDelete.Prev;
+        }
+    }
+
+    public class DoublyLinkedListNodeSM
+    {
+        public int Data { get; set; }
+
+        public DoublyLinkedListNodeSM Next;
+        public DoublyLinkedListNodeSM Prev;
+        public DoublyLinkedListNodeSM(int d)
+        {
+            Data = d;
+            Next = null;
+            Prev = null;
+        }
+    }
+}
---
+++ DoublyLinkedList/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoublyLinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ////Add a node in the end
+            //DoublyLinkedListSM doublyLinkedListSM = new DoublyLinkedListSM();
+            //doublyLinkedListSM.AddANodeInTheEnd(10);
+            //doublyLinkedListSM.AddANodeInTheEnd(20);
+            //doublyLinkedListSM.AddANodeInTheEnd(30);
+            //doublyLinkedListSM.AddANodeInTheEnd(40);
+            //doublyLinkedListSM.AddANodeInTheEnd(50);
+            //doublyLinkedListSM.PrintDoublyLinkedList();
+
+
+
+            ////Add a node after a given node
+            //DoublyLinkedListSM doublyLinkedListSM = new DoublyLinkedListSM();
+            //doublyLinkedListSM.AddANodeInTheEnd(10);
+            //doublyLinkedListSM.AddANodeInTheEnd(20);
+            //doublyLinkedListSM.AddANodeInTheEnd(30);
+            //doublyLinkedListSM.AddANodeInTheEnd(40);
+            //doublyLinkedListSM.AddANodeInTheEnd(50);
+            //doublyLinkedListSM.AddANodeAfterAGivenData(30, 33);
+            //doublyLinkedListSM.PrintDoublyLinkedList();
+
+
+
+            ////Delete a node in a Doubly Linked List
+            DoublyLinkedListSM doublyLinkedListSM = new DoublyLinkedListSM();
+            doublyLinkedListSM.AddANodeInTheEnd(10);
+            doublyLinkedListSM.AddANodeInTheEnd(20);
+            doublyLinkedListSM.AddANodeInTheEnd(30);
+            doublyLinkedListSM.AddANodeInTheEnd(40);
+            doublyLinkedListSM.AddANodeInTheEnd(50);
+            doublyLinkedListSM.DeleteANode(30);
+            doublyLinkedListSM.PrintDoublyLinkedList();
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
