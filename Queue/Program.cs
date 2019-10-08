e1ca55253c661ddb81751d7cc6c0064a69ff9837
SATYARTH MISRA <misrasatyarth@gmail.com>
2019-10-09 01:37:08 +0530

    Created the project queue

---
+++ Queue/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            QueueSM queueSM = new QueueSM(5);
+            queueSM.EnqueueSM(10);
+            queueSM.EnqueueSM(20);
+            queueSM.EnqueueSM(30);
+            queueSM.EnqueueSM(40);
+            queueSM.EnqueueSM(50);
+            Console.WriteLine(queueSM.DequeueSM()); 
+            queueSM.PrintQueueSM();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Queue/QueueSM.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queue
+{
+    public class QueueSM
+    {
+        private int[] queueElements;
+        private int max;
+        private int front;
+        private int rear;
+
+        public QueueSM(int size)
+        {
+            queueElements = new int[size];
+            max = size;
+            front = 0;
+            rear = -1;
+        }
+
+        public void EnqueueSM(int d)
+        {
+            if(rear == max - 1)
+            {
+                Console.WriteLine("queue is full");
+                return;
+            }
+            queueElements[++rear] = d;
+        }
+
+        public int DequeueSM()
+        {
+            if (front == rear+1)
+            {
+                Console.WriteLine("queue is empty");
+                return int.MinValue;
+            }
+            int element = queueElements[front];
+            front++;
+            return element;
+        }
+
+        public void PrintQueueSM()
+        {
+            if (front == rear + 1)
+            {
+                Console.WriteLine("queue is empty");
+                return;
+            }
+            while(front!=rear+1)
+            Console.WriteLine(queueElements[front++]);
+        }
+    }
+}
