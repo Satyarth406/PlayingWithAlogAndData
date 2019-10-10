b47c4eee4444d9accfe92e0d65870408fdfe9257
satyarth misra <misrasatyarth@gmail.com>
2019-10-10 14:32:16 +0530

    added some methods of BinarySearchTree

--- BinarySearchTree/BinarySearchTreeSM.cs
+++ BinarySearchTree/BinarySearchTreeSM.cs
@@ -8,12 +8,95 @@ namespace BinarySearchTree
 {
     public class BinarySearchTreeSM
     {
+        public BinarySearchTreeNodeSM Root;
+        public BinarySearchTreeNodeSM AddNode(BinarySearchTreeNodeSM node, int data)
+        {
+            if (node == null)
+            {
+                node = new BinarySearchTreeNodeSM(data);
+                return node;
+            }
+
+            if (node.Data < data)
+            {
+                node.RightChild = AddNode(node.RightChild, data);
+            }
+            else
+            {
+                node.LeftChild = AddNode(node.LeftChild, data);
+            }
+
+            return node;
+        }
+
+        public BinarySearchTreeNodeSM FindTheGivenData(BinarySearchTreeNodeSM root, int d)
+        {
+            if (root == null) return null;
+            if (root.Data == d) return root;
+            if (root.Data > d)
+            {
+                return FindTheGivenData(root.LeftChild, d);
+            }
+            else
+            {
+                return FindTheGivenData(root.RightChild, d);
+            }
+        }
+
+        public int FindTheMinValue(BinarySearchTreeNodeSM root)
+        {
+            if (root.LeftChild == null)
+            {
+                return root.Data;
+            }
+
+            return FindTheMinValue(root.LeftChild);
+        }
+
+        public void FindInorderPredecessorAndSuccessorOfKey(BinarySearchTreeNodeSM root, int i, ref BinarySearchTreeNodeSM pred, ref BinarySearchTreeNodeSM succ)
+        {
+            if(root ==null) return;
+            if (root.Data == i)
+            {
+                BinarySearchTreeNodeSM dummy = root.LeftChild;
+                while (dummy.RightChild != null)
+                {
+                    dummy = dummy.RightChild;
+                }
+
+                pred = dummy;
+                if (root.RightChild != null)
+                {
+                    dummy = dummy.LeftChild;
+                }
+
+                succ = dummy;
+                return;
+            }
+            if (root.Data > i)
+            {
+                succ = root;
+                FindInorderPredecessorAndSuccessorOfKey(root.LeftChild, i, ref pred, ref succ);
+            }
+
+            if (root.Data < i)
+            {
+                pred = root;
+                FindInorderPredecessorAndSuccessorOfKey(root.RightChild, i, ref pred, ref succ);
+            }
+        }
     }
     public class BinarySearchTreeNodeSM
     {
-        public int data;
-        public BinarySearchTreeNodeSM leftChild;
-        public BinarySearchTreeNodeSM rightChild;
+        public int Data;
+        public BinarySearchTreeNodeSM LeftChild;
+        public BinarySearchTreeNodeSM RightChild;
 
+        public BinarySearchTreeNodeSM(int d)
+        {
+            Data = d;
+            LeftChild = null;
+            RightChild = null;
+        }
     }
 }
--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -10,6 +10,60 @@ namespace BinarySearchTree
     {
         static void Main(string[] args)
         {
+
+            ////Insert a node into the binary Search tree
+            //BinarySearchTreeSM binarySearchTreeSm = new BinarySearchTreeSM();
+            //binarySearchTreeSm.Root = binarySearchTreeSm.AddNode(binarySearchTreeSm.Root,10);
+            //binarySearchTreeSm.Root = binarySearchTreeSm.AddNode(binarySearchTreeSm.Root, 3);
+            //binarySearchTreeSm.Root = binarySearchTreeSm.AddNode(binarySearchTreeSm.Root, 5);
+            //binarySearchTreeSm.Root = binarySearchTreeSm.AddNode(binarySearchTreeSm.Root, 15);
+
+
+
+
+            ////Search a given node in the binary Search tree
+            //BinarySearchTreeSM binarySearchTreeSm = new BinarySearchTreeSM();
+            //binarySearchTreeSm.Root = binarySearchTreeSm.AddNode(binarySearchTreeSm.Root, 10);
+            //binarySearchTreeSm.Root = binarySearchTreeSm.AddNode(binarySearchTreeSm.Root, 3);
+            //binarySearchTreeSm.Root = binarySearchTreeSm.AddNode(binarySearchTreeSm.Root, 5);
+            //binarySearchTreeSm.Root = binarySearchTreeSm.AddNode(binarySearchTreeSm.Root, 15);
+            //binarySearchTreeSm.Root = binarySearchTreeSm.AddNode(binarySearchTreeSm.Root, 12);
+            //BinarySearchTreeNodeSM t = binarySearchTreeSm.FindTheGivenData(binarySearchTreeSm.Root, 12);
+            //if (t != null)
+            //{
+            //    Console.WriteLine("The given key is present in the tree");
+            //}
+
+
+
+
+            ////Find the node with minimum value in a Binary Search Tree
+            //BinarySearchTreeSM binarySearchTreeSm = new BinarySearchTreeSM();
+            //binarySearchTreeSm.Root = binarySearchTreeSm.AddNode(binarySearchTreeSm.Root, 10);
+            //binarySearchTreeSm.Root = binarySearchTreeSm.AddNode(binarySearchTreeSm.Root, 3);
+            //binarySearchTreeSm.Root = binarySearchTreeSm.AddNode(binarySearchTreeSm.Root, 5);
+            //binarySearchTreeSm.Root = binarySearchTreeSm.AddNode(binarySearchTreeSm.Root, 15);
+            //binarySearchTreeSm.Root = binarySearchTreeSm.AddNode(binarySearchTreeSm.Root, 12);
+            //int min = binarySearchTreeSm.FindTheMinValue(binarySearchTreeSm.Root);
+            //Console.WriteLine(min);
+
+
+
+            ////Inorder predecessor and successor for a given key in BST
+            //BinarySearchTreeSM binarySearchTreeSm = new BinarySearchTreeSM();
+            //binarySearchTreeSm.Root = binarySearchTreeSm.AddNode(binarySearchTreeSm.Root, 10);
+            //binarySearchTreeSm.Root = binarySearchTreeSm.AddNode(binarySearchTreeSm.Root, 3);
+            //binarySearchTreeSm.Root = binarySearchTreeSm.AddNode(binarySearchTreeSm.Root, 5);
+            //binarySearchTreeSm.Root = binarySearchTreeSm.AddNode(binarySearchTreeSm.Root, 15);
+            //binarySearchTreeSm.Root = binarySearchTreeSm.AddNode(binarySearchTreeSm.Root, 12);
+            //BinarySearchTreeNodeSM pred = null, succ = null;
+            //binarySearchTreeSm.FindInorderPredecessorAndSuccessorOfKey(binarySearchTreeSm.Root, 13, ref pred, ref succ);
+            //Console.WriteLine("Predecessor is " + pred.Data + "  Successor is: " + succ.Data);
+
+
+
+
+            Console.ReadLine();
         }
     }
 }
